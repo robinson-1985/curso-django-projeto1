7aef96e1f0a6cb50e20d9c5e8321d8c826f0c582
robinson <robinpaulista85@gmail.com>
2022-05-18 19:16:54 -0300

    Criando uma classe base para os testes.

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -47,7 +47,6 @@ class RecipeViewsTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
-        assert 1 == 1
 
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
